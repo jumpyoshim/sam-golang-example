087ffff21f32e69c2869b3786ea12223f6a056c8
Jumpei Yoshimura <jumpyoshim@gmail.com>
2020-12-09 12:15:45 +0900

    update

--- funcs/user/main.go
+++ funcs/user/main.go
@@ -2,33 +2,32 @@ package main
 
 import (
 	"context"
-	"encoding/json"
+	"log"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	ginadapter "github.com/awslabs/aws-lambda-go-api-proxy/gin"
+	"github.com/gin-gonic/gin"
 )
 
-type Output struct {
-	Message string `json:"message"`
+var ginLambda *ginadapter.GinLambda
+
+func init() {
+	// stdout and stderr are sent to AWS CloudWatch Logs
+	log.Printf("Gin cold start")
+	r := gin.Default()
+	r.GET("/user/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	ginLambda = ginadapter.New(r)
 }
 
-func Handler(
-	ctx context.Context,
-	req events.APIGatewayProxyRequest, // nolint: gocritic
-) (out events.APIGatewayProxyResponse, err error) {
-	o := Output{
-		Message: "ok",
-	}
-
-	b, err := json.Marshal(o)
-	if err != nil {
-		return out, err
-	}
-
-	return events.APIGatewayProxyResponse{
-		Body:       string(b),
-		StatusCode: 200,
-	}, nil
+func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	// If no name is provided in the HTTP request body, throw an error
+	return ginLambda.ProxyWithContext(ctx, req)
 }
 
 func main() {
--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.15
 
 require (
 	github.com/aws/aws-lambda-go v1.20.0
-	github.com/awslabs/aws-lambda-go-api-proxy v0.8.1
+	github.com/awslabs/aws-lambda-go-api-proxy v0.9.0
 	github.com/gin-gonic/gin v1.6.3
 )
